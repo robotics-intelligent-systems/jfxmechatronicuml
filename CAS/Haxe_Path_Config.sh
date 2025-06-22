bf936a950f1ee12ea75411721d8eb46da42ecef7
Developer <kjsanc@protonmail.com>
2025-06-22 16:27:10 -0500

    [add] Adding haxe path config script

---
+++ CAS/Haxe_Path_Config.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# Haxe - Replace paths with your installation
+export HAXEPATH=/usr/bin/
+export HAXE_STD_PATH=/usr/share/haxe/std/
+export HAXELIB_PATH=~/development/haxelib/
+
+# Neko - Replace paths with your installation
+export NEKOPATH=/usr/bin/
+export NEKO_INSTPATH=/usr/lib/x86_64-linux-gnu/neko/
+
+exec ~/.eclipse/java-2024-09/eclipse/eclipse
